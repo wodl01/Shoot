b468179dc1a3054ec1f2226f7570c1cca570f8fb
Yeonjun Jeong <rkfkqudtlsk00@gmail.com>
2020-09-26 03:55:20 +0900

    FirstTime
    Yeahhhhhhhhhhhh

---
+++ Assets/NetWorkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetWorkManager : MonoBehaviourPunCallbacks
+{
+    public InputField NickName;
+
+    private void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickName.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+        Debug.Log("들어가는중");
+    }
+    public override void OnJoinedRoom()
+    {
+        
+    }
+    //https://www.youtube.com/watch?v=9Bn1C9O0hzY&t=236s;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Disconnect();
+            Debug.Log("연결끊김");
+        }
+
+    }
+
+}
